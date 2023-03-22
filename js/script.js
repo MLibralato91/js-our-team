4fc5a821027bce53e9f1f8573080d15fc6101479
MLibralato91 <michelelibralato911@gmail.com>
2023-03-22 14:30:39 +0100

    add object

--- js/script.js
+++ js/script.js
@@ -21,6 +21,43 @@ Organizzare i singoli membri in card/schede
 
 
 
+const myteam = [
+    {
+        name: 'Wayne',
+        surname : 'Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela',
+        surname : 'Caroll',
+        role: 'Founder & CEOChief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter',
+        surname : 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela',
+        surname : 'Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott',
+        surname : 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara',
+        surname : 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
 
-
-
+console.log();
